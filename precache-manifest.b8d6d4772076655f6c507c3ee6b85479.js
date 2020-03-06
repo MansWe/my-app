72f4d83c1454eb33a08c27601431b574c0acea83
MansWe <manswenning@gmail.com>
2020-03-06 10:34:58 +0100

    Updates

---
+++ precache-manifest.b8d6d4772076655f6c507c3ee6b85479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fcb915d60338e6ddcc90467150483e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6ba5d63058e644d4e050",
+    "url": "/my-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b8045c8ef84ba516d4e8",
+    "url": "/my-app/static/css/main.fb443a02.chunk.css"
+  },
+  {
+    "revision": "6ba5d63058e644d4e050",
+    "url": "/my-app/static/js/2.f6570ed8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-app/static/js/2.f6570ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8045c8ef84ba516d4e8",
+    "url": "/my-app/static/js/main.022c2018.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "fa868f0403b3f9ec5ed178e362ffdb34",
+    "url": "/my-app/static/media/Me.fa868f04.png"
+  }
+]);
